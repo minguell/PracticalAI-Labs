ef78b7848a02911519a423f9e779040ca9d22d06
PLLima <74931964+PLLima@users.noreply.github.com>
2024-10-30 12:19:28 -0300

    feat: add step_gradient function (to be tested)

--- T1/alegrete.py
+++ T1/alegrete.py
@@ -1,5 +1,4 @@
 import numpy as np
-import csv  
 
 def compute_mse(b, w, data):
     """
@@ -13,25 +12,37 @@ def compute_mse(b, w, data):
     y = data[:,1]
     n = len(data)
     mse = 0
-    for i in range (n):
+    for i in range(n):
         mse += (y[i] - (w*x[i] + b))**2
     mse = mse/n
-    print(f"Mean Squared Error: ", mse)
-    return mse    
-    
+
+    return mse     
 
 
 def step_gradient(b, w, data, alpha):
     """
-    Executa uma atualização por descida do gradiente  e retorna os valores atualizados de b e w.
+    Executa uma atualização por descida do gradiente e retorna os valores atualizados de b e w.
     :param b: float - bias (intercepto da reta)
     :param w: float - peso (inclinacao da reta)
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de b e w, respectivamente
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    x = data[:,0]
+    y = data[:,1]
+    n = len(data)
+    part_deriv_b = 0
+    part_deriv_w = 0
+    for i in range(n):
+        part_deriv_b += -2*(y[i] - (w*x[i] + b))
+        part_deriv_w += -2*x[i]*(y[i] - (w*x[i] + b))
+    part_deriv_b = part_deriv_b/n
+    part_deriv_w = part_deriv_w/n
 
+    new_b = b - alpha*part_deriv_b
+    new_w = w - alpha*part_deriv_w
+
+    return new_b, new_w
 
 def fit(data, b, w, alpha, num_iterations):
     """
