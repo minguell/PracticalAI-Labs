d213e36365e90cb3ccfdc34a8def2ed086c98299
PLLima <74931964+PLLima@users.noreply.github.com>
2024-12-21 16:38:14 -0300

    feat: finish mcts implementation (not tested)

--- T4/kit_games/advsearch/your_agent/mcts.py
+++ T4/kit_games/advsearch/your_agent/mcts.py
@@ -1,5 +1,6 @@
 import random
 import math
+import time
 from typing import Tuple
 
 def ucb(node):
@@ -34,9 +35,10 @@ class mcts_node:
     Class to represent a node in the MCTS tree. Each node has a state,
     parent, wins, visits and children
     """
-    def __init__(self, state, parent=None, wins=0, visits=0, children=[]):
+    def __init__(self, state, parent=None, parent_move=(-1, -1), wins=0, visits=0, children=[]):
         self.state = state
         self.parent = parent
+        self.parent_move = parent_move
         self.wins = wins
         self.visits = visits
         self.children = children
@@ -64,11 +66,29 @@ class mcts_node:
         Expands the current node and returns the first non-visited child
         :return: next non-visited child or None if node is already expanded
         """
-        for move in self.state.legal_moves():
-            if move not in self.children:
-                self.children.append(move)
-                return mcts_node(self.state.next_state(move), self)
+        # Discover the movements of children
+        children_moves = set()
+        for child in self.children:
+            children_moves.add(child.parent_move)
+
+        # Check if a move is already in the movements of children
+        legal_moves = set(self.state.legal_moves())
+        for move in legal_moves:
+            if move not in children_moves:
+                new_child = mcts_node(self.state.next_state(move), self, move)
+                self.children.append(child)
+                return new_child
         return None
+
+    def get_root(self):
+        """
+        Returns the root of the tree
+        :return: root of the tree
+        """
+        node = self
+        while node.parent is not None:
+            node = node.parent
+        return node
 
     def simulate_game(self):
         """
@@ -76,20 +96,54 @@ class mcts_node:
         getting the result of the match
         :return: result of the match (1 for win, 0 for draw, -1 for loss)
         """
+        # Discover the player of the root of the tree
+        player = self.get_root().state.player
+
+        # Simulate the rest of the game randomly
         state = self.state
         while not state.is_terminal():
             move = random.choice(state.legal_moves())
             state = state.next_state(move)
-        self.visits += 1
         winner = state.winner()
-        if winner == self.state.player:
-            self.wins += 1
+        if winner == player:
             return 1
         elif winner is None:
             return 0
         else:
-            self.wins -= 1
             return -1
+
+    def backpropagate(self, result):
+        """
+        Backpropagates the result of the last match to parent nodes
+        :param result: result of the match (1 for win, 0 for draw, -1 for loss)
+        """
+        # Discover the player of the root of the tree
+        player = self.get_root().state.player
+
+        # Backprogate the result to the parent nodes (respecting the player of each node)
+        node = self
+        while node is not None:
+            node.visits += 1
+            node_player = node.state.player
+            if node_player == player:
+                node.wins += result
+            else:
+                node.wins -= result
+            node = node.parent
+
+    def get_best_move(self) -> Tuple[int, int]:
+        """
+        Returns the best move for the current node
+        :return: (int, int) tuple with x, y coordinates of the best move (remember: 0 is the first row/column)
+        """
+        best_score = float('-inf')
+        best_move = (-1, -1)
+        for child in self.children:
+            score = child.wins / child.visits
+            if score > best_score:
+                best_score = score
+                best_move = child.parent_move
+        return best_move
 
 def make_move(state) -> Tuple[int, int]:
     """
@@ -100,8 +154,17 @@ def make_move(state) -> Tuple[int, int]:
     :param state: state to make the move
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
-
-    # o codigo abaixo retorna uma jogada ilegal
-    # Remova-o e coloque a sua implementacao do MCTS
-
-    return (-1, -1)
+    max_iterations = 20
+    max_time = 5
+    initial_time = time.time()
+    spent_time = 0
+    iterations = 0
+    root = mcts_node(state)
+    while spent_time < max_time and iterations > max_iterations:
+        node = root.select_node()
+        node = node.expand_node()
+        result = node.simulate_game()
+        node.backpropagate(result)
+        iterations += 1
+        spent_time = time.time() - initial_time
+    return root.get_best_move()
