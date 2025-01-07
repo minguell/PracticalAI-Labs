63482871eea46c3fc60598697bc000588932fd29
Ana Rodrigues <rodrigues.ac2001@gmail.com>
2025-01-07 11:30:12 -0300

    feat: implement tttm minimax

--- T4/kit_games/advsearch/dOnaThello/tttm_minimax.py
+++ T4/kit_games/advsearch/dOnaThello/tttm_minimax.py
@@ -26,11 +26,11 @@ def make_move(state: GameState) -> Tuple[int, int]:
     # uma vez que o jogo tem profundidade maxima 9. 
     # Preencha a funcao utility com o valor de um estado terminal e passe-a como funcao de avaliação para seu minimax_move
    
-    _, best_move = minimax_alpha_beta(state, depth=-1, alpha=-math.inf, beta=math.inf, maximizing=True,player=state.player)
-    return best_move
+    return minimax_move(state, max_depth = -1, eval_func = utility)
 
+"""
 def minimax_alpha_beta(state: GameState, depth: int, alpha: float, beta: float, maximizing: bool, player: str) -> Tuple[float, Tuple[int, int]]:
-    """
+    
     Implementação do algoritmo Minimax com poda alfa-beta.
     :param state: estado atual do jogo
     :param depth: profundidade máxima (-1 para ilimitada)
@@ -39,7 +39,6 @@ def minimax_alpha_beta(state: GameState, depth: int, alpha: float, beta: float, 
     :param maximizing: True se for a vez do jogado; False, caso contrário
     :param player: jogador para quem estamos calculando o valor ('B' ou 'W')
     :return: Tupla (valor da avaliação, melhor movimento)
-    """
 
     if state.is_terminal():
         return utility(state, state.player), None
@@ -55,7 +54,7 @@ def minimax_alpha_beta(state: GameState, depth: int, alpha: float, beta: float, 
                 max_eval = eval
                 best_move = move 
             alpha = max(alpha, eval)
-            if beta <= alpha:
+            if beta < alpha:
                 break
         return max_eval, best_move
     else:
@@ -67,10 +66,11 @@ def minimax_alpha_beta(state: GameState, depth: int, alpha: float, beta: float, 
                 min_eval = eval
                 best_move = move
             beta = min(beta, eval)
-            if beta <= alpha:
+            if beta < alpha:
                 break
         return min_eval, best_move
-
+"""
+        
 def utility(state, player:str) -> float:
     """
     Retorna a utilidade de um estado (terminal) 
@@ -78,10 +78,11 @@ def utility(state, player:str) -> float:
     winner = state.winner()
 
     if winner == player:
-        return -1
+        return 1
     
-    elif winner != 'D' and winner is not None:
-        return -1
+    elif winner == None:
+        return 0
     
-    return 0
+    else:
+        return -1
 
