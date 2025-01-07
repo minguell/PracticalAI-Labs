e00e9f5fc45e55c2ee29da1eada962c17384e9aa
Ana Rodrigues <rodrigues.ac2001@gmail.com>
2025-01-07 16:05:56 -0300

    feat: implement tournament agent

--- T4/kit_games/advsearch/dOnaThello/tournament_agent.py
+++ T4/kit_games/advsearch/dOnaThello/tournament_agent.py
@@ -2,13 +2,7 @@ import random
 from typing import Tuple
 from ..othello.gamestate import GameState
 from ..othello.board import Board
-
-# Voce pode criar funcoes auxiliares neste arquivo
-# e tambem modulos auxiliares neste pacote.
-#
-# Nao esqueca de renomear 'your_agent' com o nome
-# do seu agente.
-
+from .othello_minimax_custom import make_move as custom
 
 def make_move(state) -> Tuple[int, int]:
     """
@@ -19,12 +13,4 @@ def make_move(state) -> Tuple[int, int]:
     :param state: state to make the move
     :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
     """
-
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada 
-    # Remova-o e coloque a sua implementacao da poda alpha-beta
-
-    if state.game_name == 'Othello':
-        return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
-
-
+    return custom(state)
